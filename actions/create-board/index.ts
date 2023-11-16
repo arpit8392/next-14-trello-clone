377b22c89bcdd4f987c895d278d90e145a11b45c
Arpit Namdev <arpit8392@gmail.com>
2023-11-16 17:41:28 +0530

    useAction abstraction

---
+++ actions/create-board/index.ts
@@ -0,0 +1,57 @@
+'use server'
+
+import { createSafeAction } from '@/lib/create-safe-action'
+import { auth } from '@clerk/nextjs'
+import { CreateBoard } from './schema'
+import { InputType, ReturnType } from './types'
+import { revalidatePath } from 'next/cache'
+import prisma from '@/lib/db'
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+	const { userId, orgId } = auth()
+
+	if (!userId || !orgId) {
+		return {
+			error: 'Unauthorized',
+		}
+	}
+
+	const { title, image } = data
+
+	const [imageId, imageThumbUrl, imageFullUrl, imageLinkHTML, imageUserName] =
+		image.split('|')
+  
+    if(!imageId || !imageThumbUrl || !imageFullUrl || !imageUserName || !imageLinkHTML) {
+      return {
+        error: 'Missing fields. Failed to create board.'
+      }
+    }
+
+    let board
+
+    try {
+      board = await prisma.board.create({
+        data: {
+          title,
+          orgId,
+          imageId,
+          imageThumbUrl,
+          imageFullUrl,
+          imageUserName,
+          imageLinkHTML
+        }
+      })
+
+    } catch (error) {
+      return {
+        error: 'Failed to create.'
+      }
+    }
+
+    revalidatePath(`/board/${board.id}`)
+    return {
+      data: board
+    }
+}
+
+export const createBoard = createSafeAction(CreateBoard, handler)
