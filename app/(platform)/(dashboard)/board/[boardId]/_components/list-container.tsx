136fbd8f7c16155f453dfe3f2293e6bca9c3dd11
Arpit Namdev <arpit8392@gmail.com>
2023-11-26 08:58:39 +0530

    Create List

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { ListWithCards } from '@/types'
+import { useState } from 'react'
+import { ListForm } from './list-form'
+
+interface ListContainerProps {
+	data: ListWithCards[]
+	boardId: string
+}
+
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+	const [orderedData, setOrderedData] = useState(data)
+	return (
+		<div>
+			<ol className='flex gap-3 h-full'>
+				{/* {orderedData.map((list, index) => (
+          <ListItem key={list.id} index={index} data={list} />
+        ))} */}
+				{/* TODO: List Form */}
+				<ListForm />
+				<div className='flex-shrink-0 w-1' />
+			</ol>
+		</div>
+	)
+}
--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,7 @@
+import prisma from '@/lib/db'
 import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
+import { ListContainer } from './_components/list-container'
 
 interface BoardIdPageProps {
 	params: {
@@ -14,6 +16,27 @@ const BoardIdPage = async ({ params }: BoardIdPageProps) => {
 		redirect('/select-org')
 	}
 
-	return <div className='p-4 h-full overflow-x-auto'></div>
+	const lists = await prisma.list.findMany({
+		where: {
+			boardId: params.boardId,
+			board: {
+				orgId
+			}
+		},
+		include: {
+			cards: {
+				orderBy: {
+					order: 'asc'
+				}
+			}
+		},
+		orderBy: {
+			order: 'asc'
+		}
+	})
+
+	return <div className='p-4 h-full overflow-x-auto'>
+		<ListContainer boardId={params.boardId} data={lists} />
+	</div>
 }
 export default BoardIdPage
