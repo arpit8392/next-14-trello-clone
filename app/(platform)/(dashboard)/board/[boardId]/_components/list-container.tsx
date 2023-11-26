5d404dc26224029d50ca8895aed11410be916f28
Arpit Namdev <arpit8392@gmail.com>
2023-11-26 09:40:49 +0530

    List Header

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -3,6 +3,7 @@
 import { ListWithCards } from '@/types'
 import { useState } from 'react'
 import { ListForm } from './list-form'
+import { ListItem } from './list-item'
 
 interface ListContainerProps {
 	data: ListWithCards[]
@@ -14,10 +15,9 @@ export const ListContainer = ({ data, boardId }: ListContainerProps) => {
 	return (
 		<div>
 			<ol className='flex gap-3 h-full'>
-				{/* {orderedData.map((list, index) => (
-          <ListItem key={list.id} index={index} data={list} />
-        ))} */}
-				{/* TODO: List Form */}
+				{orderedData.map((list, index) => (
+					<ListItem key={list.id} index={index} data={list} />
+				))}
 				<ListForm />
 				<div className='flex-shrink-0 w-1' />
 			</ol>
